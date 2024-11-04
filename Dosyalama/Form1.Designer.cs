432a5d99a9263a8c4fed03514f2cf17f32b6303b
BerkhanSaral <137205976+BerkhanSaral@users.noreply.github.com>
2024-11-04 10:52:48 +0300

    Add project files.

---
+++ Dosyalama/Form1.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace Dosyalama
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtOkulNo = new System.Windows.Forms.TextBox();
+            this.txtAdSoyad = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtVize = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtFinal = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cmbDersAdi = new System.Windows.Forms.ComboBox();
+            this.btnKaydet = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.lstOkulNo = new System.Windows.Forms.ListBox();
+            this.lstDersAdi = new System.Windows.Forms.ListBox();
+            this.lstOrt = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(71, 83);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Okul No";
+            // 
+            // txtOkulNo
+            // 
+            this.txtOkulNo.Location = new System.Drawing.Point(170, 79);
+            this.txtOkulNo.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtOkulNo.Name = "txtOkulNo";
+            this.txtOkulNo.Size = new System.Drawing.Size(148, 26);
+            this.txtOkulNo.TabIndex = 1;
+            // 
+            // txtAdSoyad
+            // 
+            this.txtAdSoyad.Location = new System.Drawing.Point(170, 125);
+            this.txtAdSoyad.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtAdSoyad.Name = "txtAdSoyad";
+            this.txtAdSoyad.Size = new System.Drawing.Size(148, 26);
+            this.txtAdSoyad.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(58, 129);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Ad Soyad";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(66, 175);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Ders Adi";
+            // 
+            // txtVize
+            // 
+            this.txtVize.Location = new System.Drawing.Point(170, 216);
+            this.txtVize.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtVize.Name = "txtVize";
+            this.txtVize.Size = new System.Drawing.Size(148, 26);
+            this.txtVize.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(96, 220);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Vize";
+            // 
+            // txtFinal
+            // 
+            this.txtFinal.Location = new System.Drawing.Point(170, 264);
+            this.txtFinal.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtFinal.Name = "txtFinal";
+            this.txtFinal.Size = new System.Drawing.Size(148, 26);
+            this.txtFinal.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(93, 268);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(43, 20);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Final";
+            // 
+            // cmbDersAdi
+            // 
+            this.cmbDersAdi.FormattingEnabled = true;
+            this.cmbDersAdi.Items.AddRange(new object[] {
+            "Hayat  Bilgisi",
+            "Turkce",
+            "Matematik",
+            "Beden Egitimi",
+            "Sosyal Bilgiler"});
+            this.cmbDersAdi.Location = new System.Drawing.Point(170, 172);
+            this.cmbDersAdi.Name = "cmbDersAdi";
+            this.cmbDersAdi.Size = new System.Drawing.Size(148, 28);
+            this.cmbDersAdi.TabIndex = 10;
+            // 
+            // btnKaydet
+            // 
+            this.btnKaydet.Location = new System.Drawing.Point(185, 352);
+            this.btnKaydet.Name = "btnKaydet";
+            this.btnKaydet.Size = new System.Drawing.Size(108, 38);
+            this.btnKaydet.TabIndex = 11;
+            this.btnKaydet.Text = "Kaydet";
+            this.btnKaydet.UseVisualStyleBackColor = true;
+            this.btnKaydet.Click += new System.EventHandler(this.btnKaydet_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(554, 35);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(65, 20);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Okul No";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(761, 35);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(70, 20);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Ders Adi";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(951, 35);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(126, 20);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Ortalama/Durum";
+            // 
+            // lstOkulNo
+            // 
+            this.lstOkulNo.FormattingEnabled = true;
+            this.lstOkulNo.ItemHeight = 20;
+            this.lstOkulNo.Location = new System.Drawing.Point(511, 83);
+            this.lstOkulNo.Name = "lstOkulNo";
+            this.lstOkulNo.Size = new System.Drawing.Size(163, 404);
+            this.lstOkulNo.TabIndex = 15;
+            // 
+            // lstDersAdi
+            // 
+            this.lstDersAdi.FormattingEnabled = true;
+            this.lstDersAdi.ItemHeight = 20;
+            this.lstDersAdi.Location = new System.Drawing.Point(719, 80);
+            this.lstDersAdi.Name = "lstDersAdi";
+            this.lstDersAdi.Size = new System.Drawing.Size(163, 404);
+            this.lstDersAdi.TabIndex = 16;
+            // 
+            // lstOrt
+            // 
+            this.lstOrt.FormattingEnabled = true;
+            this.lstOrt.ItemHeight = 20;
+            this.lstOrt.Location = new System.Drawing.Point(933, 79);
+            this.lstOrt.Name = "lstOrt";
+            this.lstOrt.Size = new System.Drawing.Size(163, 404);
+            this.lstOrt.TabIndex = 17;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1209, 493);
+            this.Controls.Add(this.lstOrt);
+            this.Controls.Add(this.lstDersAdi);
+            this.Controls.Add(this.lstOkulNo);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btnKaydet);
+            this.Controls.Add(this.cmbDersAdi);
+            this.Controls.Add(this.txtFinal);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtVize);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtAdSoyad);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtOkulNo);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtOkulNo;
+        private System.Windows.Forms.TextBox txtAdSoyad;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtVize;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtFinal;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox cmbDersAdi;
+        private System.Windows.Forms.Button btnKaydet;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.ListBox lstOkulNo;
+        private System.Windows.Forms.ListBox lstDersAdi;
+        private System.Windows.Forms.ListBox lstOrt;
+    }
+}
+
---
+++ Dosyalama/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Dosyalama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string yol = Application.StartupPath + "\\notlar.txt";
+        private void btnKaydet_Click(object sender, EventArgs e)
+        {
+
+            StreamWriter dosya = File.AppendText(yol);
+          
+            dosya.Write(txtOkulNo.Text + " - ");
+            dosya.Write(txtAdSoyad.Text + " - ");
+            dosya.Write(cmbDersAdi.Text + " - ");
+            dosya.Write(txtVize.Text + " - ");
+            dosya.Write(txtFinal.Text + " - ");
+
+           
+            double ort = Convert.ToDouble(txtVize.Text) * 0.4 + Convert.ToDouble(txtFinal.Text) * 0.6;
+
+
+            if (ort < 50)
+            {
+                dosya.WriteLine(ort.ToString() + " FF/Kaldi");
+            }
+            else if (ort > 49 && ort < 70)
+            {
+                dosya.WriteLine(ort.ToString() + " CC/Gecti");
+            }
+            else if (ort > 69 && ort < 84)
+            {
+                dosya.WriteLine(ort.ToString() + " BB/Gecti");
+            }
+            else if (ort > 83)
+            {
+                dosya.WriteLine(ort.ToString() + " AA/Gecti");
+            }
+
+          
+
+            dosya.Close();
+            MessageBox.Show("Kayit Tamamlandi");
+
+
+            foreach (Control nesne in this.Controls)
+            {
+                if (nesne is TextBox)
+                {
+                    ((TextBox)nesne).Clear();
+                }
+            }
+            cmbDersAdi.Text = "";
+            txtOkulNo.Focus();
+            listele();
+
+        }
+        public void listele()
+        {
+            lstOkulNo.Items.Clear();
+            lstDersAdi.Items.Clear();
+            lstOrt.Items.Clear();
+            if (File.Exists(yol))
+            {
+               StreamReader dosya=File.OpenText(yol);
+                string oku = dosya.ReadLine();
+                while (oku!=null)
+                {
+                    string[] parca = oku.Split('-');
+                    lstOkulNo.Items.Add(parca[0]);
+                    lstDersAdi.Items.Add(parca[2]);
+                    lstOrt.Items.Add(parca[5]);
+                    oku = dosya.ReadLine();
+                }
+                dosya.Close();
+            }
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listele();
+        }
+    }
+}
